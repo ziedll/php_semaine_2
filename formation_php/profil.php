4418aaa5f4bc53d5c5f4c200471f7c1c1bb00dd6
ziedll <112785620+ziedll@users.noreply.github.com>
2026-03-09 12:32:36 +0100

    first commit

---
+++ formation_php/profil.php
@@ -0,0 +1,9 @@
+<?php
+$prenom ="Maxime";
+$entreprise ="Tyrolium";
+$annee = 2026;
+echo "Formateur : $prenom \n";
+echo "<br>";
+echo "Structure : $entreprise \n";
+echo "<br>";
+echo  "Année : $annee \n";
